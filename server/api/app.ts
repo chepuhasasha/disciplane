300bee1bd6713aab0761cd7dc60a250c7c82ba63
Sergey Batukov <sashachepuha@gmail.com>
2022-01-21 20:45:40 +0300

    update

---
+++ server/api/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { userRouter } from "./users/users.js";
+
+const port = 8000;
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(`time: ${Date.now()}`);
+  next();
+});
+app.use("/users", userRouter);
+
+app.get("/hello", (req, res) => {
+  throw new Error("Errrrrrror!");
+});
+app.post("/hello", (req, res) => {
+  res.send("CASHCOW API");
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(500).send(err.message);
+});
+
+app.listen(port, () => {
+  console.log(port);
+});
