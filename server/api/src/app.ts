4cb4b2286544f666788c7d005ed3eae2fa860237
Sergey Batukov <sashachepuha@gmail.com>
2022-01-23 11:09:49 +0300

    add simple logger service

---
+++ server/api/src/app.ts
@@ -0,0 +1,26 @@
+import express, { Express } from "express";
+import { Server } from "http";
+import { LoggerService } from "./logger/logger.servece";
+import { userRouter } from "./users/users";
+
+export class App {
+  app: Express;
+  port: number;
+  server: Server;
+  logger: LoggerService;
+  constructor(logger: LoggerService) {
+    this.app = express();
+    this.port = 8000;
+    this.logger = logger;
+  }
+
+  useRoutes() {
+    this.app.use("/users", userRouter);
+  }
+
+  public async init() {
+    this.useRoutes();
+    this.server = this.app.listen(this.port);
+    this.logger.log(this.port);
+  }
+}
